cba2b93f8abc2664128f2a58d71edafab03621fa
HeraBeginners25 <gallerysingaraja8@gmail.com>
2025-10-22 19:22:17 +0800

    add scroll horizon

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
