d32300210ee348c6e409e0e20f8e80236bba6c19
HeraBeginners25 <gallerysingaraja8@gmail.com>
2025-12-17 16:19:35 +0800

    update routes

--- resources/views/detail-cerpen.blade.php
+++ resources/views/detail-cerpen.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $cerpen->title }} - AM Smantiara</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
     <link rel="shortcut icon"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // 👇 Tambahkan baris ini
-  darkMode: 'class', 
-
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-        display: ['Syne', 'sans-serif'],
-        serif: ['Playfair Display', 'serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
